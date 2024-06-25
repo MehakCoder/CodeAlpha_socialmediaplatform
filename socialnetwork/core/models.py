e97dc2e027b79aa37ae66b79e9a58d66d549b808
Mehak <cmehak26@gmail.com>
2024-06-25 11:04:11 +0530

    add image and do same setting

--- socialnetwork/core/models.py
+++ socialnetwork/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profileing = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = pass
+   
