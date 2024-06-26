3fca14837c25d1353586bffb357029ed5c904c7d
Mehak <cmehak26@gmail.com>
2024-06-26 14:39:52 +0530

    add in signin value

--- socialnetwork/core/views.py
+++ socialnetwork/core/views.py
@@ -10,42 +10,46 @@ def index(request):
 
 def signup(request):
 
-   if request.method == 'Post' :
-       username = request.Post['username']
-       email = request.Post['email']
-       password = request.Post['Password']
-       password2 = request.Post['Password 2']
-   
-       if password == password2:
-          if User.objects.filter(email=email).exists():
-             messages.info(request,'Email Taken') 
-             return redirect('signup') 
-          elif User.objects.filter(username=username).exists():
-               messages.info(request, 'Username Taken')
-               return redirect('signup')  
-          else:
-              user = User.objects.create_user(username=username, email=email, password=password)
-              user.save()
-              
-              #log user in and redirect to setting page
+  if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-              #create a profile object for the new user
-              user_model = User.objects.get(username=username)
-              new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
-              new_profile.save()
-              return redirect('signup')
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken ')
+                return redirect('signup')   
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
 
-       else:
-         messages.info(request, 'Password Not Matching')
-         return redirect('signup')
-   else:
-      return render(request, 'signup.html')
+                
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+               
+        
+  else:
+        return render(request, 'signup.html')
    
 def signin(request):
      
      if request.method == 'POST':
          username = request.POST['username']
          password = request.POST['password']
+
+         user = auth.authenticate(username=username, password=password)
+
+         if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+         else:
+             messages.info(request, 'Credentials Invalid')
+             return redirect('signin') 
      else:    
         return render(request, 'signin.html') 
   
