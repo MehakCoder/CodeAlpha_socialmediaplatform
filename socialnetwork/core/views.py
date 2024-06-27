42d56b6fbca708793327bc31b7dc8a5d2d54a5a1
Mehak <cmehak26@gmail.com>
2024-06-27 10:35:23 +0530

    do some update in the folders

--- socialnetwork/core/views.py
+++ socialnetwork/core/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html') 
+
+@login_required(login_url='signin')
+def settings(request):
+     user_profile = Profile.object.get(user=request.user)
+     return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
 
@@ -27,7 +34,15 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -52,4 +67,8 @@ def signin(request):
              return redirect('signin') 
      else:    
         return render(request, 'signin.html') 
-  
+
+@login_required(login_url='signin')
+def logout(request):
+                auth.logout(request)
+                return redirect('signin')
